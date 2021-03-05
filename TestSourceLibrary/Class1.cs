103809e1c2d77494d6edde8068932a20220de6b5
Balaji Gunasekaran <balaji.gunasekaran@live.com>
2021-03-05 14:21:35 +0100

    Add project files.

---
+++ TestSourceLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using System.Net;
+
+namespace TestSourceLibrary
+{
+    [Route("test")]
+    public partial class Class1 : Controller
+    {
+        [HttpPost("search")]
+        [Authorize]
+        public IEnumerable<int> Test()
+        {
+            return null;
+        }
+
+        [HttpPost("search")]
+        public Task<IEnumerable<int>> Test1()
+        {
+            return null;
+        }
+    }
+}
---
+++ TestSourceLibrary/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proarc.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
